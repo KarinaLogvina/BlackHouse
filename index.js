49ed908284c9ef573c636036e3c42704b430f565
Karina Logvina <karinalogvina@Karinas-MacBook-Pro.local>
2020-10-19 07:55:57 +0300

    Added booking form

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ showSlides();
 function showSlides() {
     let i;
     const slides = document.getElementsByClassName("slide");
+    if(!slides || !slides.length) return
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -24,4 +25,26 @@ function showSlides() {
 
    slides[slideIndex-1].style.display = "block";
    setTimeout(showSlides, 5000);
+}
+
+const about = document.getElementById('about');
+if (about) {about.onclick = () => {
+    window.location.href = 'https://blackhouseclub.by/about/';
+}
+}
+
+
+const booking = document.getElementById('booking');
+const modalWrapper = document.getElementsByClassName('modal');
+const closeButton = document.getElementById('close');
+
+booking.onclick = () => {
+    modalWrapper[0].style.display = 'flex';
+    document.getElementsByTagName('main')[0].classList.add('filter');
+}
+
+closeButton.onclick = () => {
+    modalWrapper[0].style.display = 'none';
+    document.getElementsByTagName('main')[0].classList.remove('filter');
+
 }
