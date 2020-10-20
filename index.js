3f61788c87020a142fc2182d932b734bc2ea9c12
Karina Logvina <karinalogvina@Karinas-MacBook-Pro.local>
2020-10-20 20:47:17 +0300

    Added toTop animation

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-up.onclick = function() {
-    window.scrollTo(pageXOffset, 0);
-  };
+function scrollTo(to, duration = 700) {
+    const
+        element = document.scrollingElement || document.documentElement,
+        start = element.scrollTop,
+        change = to - start,
+        startDate = +new Date(),
+        easeInOutQuad = function (t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        },
+        animateScroll = function () {
+            const currentDate = +new Date();
+            const currentTime = currentDate - startDate;
+            element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
+            if (currentTime < duration) {
+                requestAnimationFrame(animateScroll);
+            }
+            else {
+                element.scrollTop = to;
+            }
+        };
+    animateScroll();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    let btn = document.getElementById('up');
+    btn.onclick = function (click) {
+        click.preventDefault();
+        scrollTo(0, 400);
+    }
+});
 
   window.addEventListener('scroll', function() {
     up.hidden = (pageYOffset < document.documentElement.clientHeight);
